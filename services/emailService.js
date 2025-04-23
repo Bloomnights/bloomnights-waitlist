03077f2589bf0638db5bdd6af768891efd6253e9
Ebenezer Akpas <akpasebenezer0@gmail.com>
2025-04-23 12:17:24 +0100

    Update Email User

--- services/emailService.js
+++ services/emailService.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 exports.sendEmail = async (to, subject, htmlContent) => {
     const transporter = nodemailer.createTransport({
-        host: "smtp.hostinger.com",
+        host: "chainconsults.com",
         port: 465,
         secure: true, // Use `true` for port 465, `false` for all other ports
         auth: {
@@ -13,7 +13,7 @@ exports.sendEmail = async (to, subject, htmlContent) => {
     });
 
     const mailOptions = {
-        from: `Bloomnights <${process.env.EMAIL_USER}>`,
+        from: `Bloomnights <bloomnights.com>`,
         to,
         subject,
         html: htmlContent,
