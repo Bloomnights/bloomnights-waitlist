1051de17c9233c73fd0b1ec99e6883f7ae419430
Ebenezer Akpas <akpasebenezer0@gmail.com>
2025-04-24 10:37:11 +0100

    Update Mailing Credentials

--- services/emailService.js
+++ services/emailService.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 exports.sendEmail = async (to, subject, htmlContent) => {
     const transporter = nodemailer.createTransport({
-        host: "chainconsults.com",
-        port: 465,
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
         secure: true, // Use `true` for port 465, `false` for all other ports
         auth: {
             user: process.env.EMAIL_USER,
